d9e0ab44bbf662fdece1598432869c7f0befc5c5
archbong <archibong.chinonso.samuel@gmail.com>
2024-05-16 16:10:25 +0100

    Create the spices product schema

--- lib/model.ts
+++ lib/model.ts
@@ -69,5 +69,64 @@ const postSchema = new mongoose.Schema(
   { timestamps: true }
 );
 
+const productSchema = new mongoose.Schema(
+  {
+    _id: {
+      type: String,
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    description: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    category: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    origin: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    medicinalProperties: [
+      {
+        type: String,
+        trim: true,
+      },
+    ],
+    culinaryUses: [
+      {
+        type: String,
+        trim: true,
+      },
+    ],
+    price: {
+      type: Number,
+      required: true,
+      min: 0,
+    },
+    stock: {
+      type: Number,
+      required: true,
+      min: 0,
+    },
+    images: [
+      {
+        type: String,
+        trim: true,
+      },
+    ],
+  },
+  { timestamps: true }
+);
+
+export const Product = mongoose.models.Product || mongoose.model("Product", productSchema);
 export const User = mongoose.models?.User || mongoose.model("User", userSchema);
 export const Post = mongoose.models?.Post || mongoose.model("Post", postSchema);
